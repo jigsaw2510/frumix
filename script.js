3c6f665ceec255c35dacc133843f067018027bc3
Catalin Gavrila <cata_bb92@yahoo.com>
2024-09-21 21:33:58 +0300

    fixed footer behaviour bug

--- script.js
+++ script.js
@@ -19,19 +19,19 @@ function myFunction() {
     
 // JavaScript to change the class based on content height and window height
     function adjustClass() {
-      var footer = document.getElementById("footer");
-      var contentHeight = document.body.clientHeight; // Height of the content
-      var windowHeight = window.innerHeight; // Height of the window
+    var footer = document.getElementById("footer");
+    var contentHeight = document.body.scrollHeight; // Total content height, including padding
+    var windowHeight = window.innerHeight; // Height of the window
 
-      if (contentHeight < windowHeight) {
+    if (contentHeight <= windowHeight) {
         footer.className = "footer-fixed";
-      } else {
+    } else {
         footer.className = "footer-relative";
-      }
     }
+}
 
-    // Initial adjustment on page load
-    adjustClass();
+// Initial adjustment on page load
+adjustClass();
 
-    // Event listener for window resize with debounce
-    window.addEventListener("resize", debounce(adjustClass, 250)); // Adjust the delay as needed
+// Event listener for window resize with debounce
+window.addEventListener("resize", debounce(adjustClass, 250));
