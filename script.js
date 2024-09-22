6414843ba3c675cd09861182f289a99ad92f65ad
Catalin Gavrila <cata_bb92@yahoo.com>
2024-09-22 22:52:30 +0300

    updated links for fb and wts to open in their respective apps on mobile devices

--- script.js
+++ script.js
@@ -47,3 +47,35 @@ adjustClass();
 
 // Event listener for window resize with debounce
 window.addEventListener("resize", debounce(adjustClass, 250));
+
+// Event listener for accessing whatsapp on desktop vs mobile
+document.addEventListener("DOMContentLoaded", function() {
+    var whatsappLink = document.getElementById("whatsapp-link");
+    var phoneNumber = "40766591635"; // Your phone number with country code (no plus sign)
+
+    // Check if the user is on a mobile device
+    if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+        whatsappLink.href = "https://wa.me/" + phoneNumber;
+    } else {
+        whatsappLink.href = "https://web.whatsapp.com/send?phone=" + phoneNumber;
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var facebookLink = document.getElementById("facebook-link");
+    var facebookPageURL = "https://www.facebook.com/frumix.ro"; // Your Facebook page URL
+    var facebookDeepLink = "fb://page/320369184498044"; // Your Facebook page ID in the deep link format
+
+    // Check if the user is on a mobile device
+    if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+        facebookLink.href = facebookDeepLink;
+        facebookLink.addEventListener("click", function(e) {
+            // Attempt to open in the Facebook app
+            setTimeout(function() {
+                window.location.href = facebookPageURL; // Fallback to browser if app is not installed
+            }, 25);
+        });
+    } else {
+        facebookLink.href = facebookPageURL;
+    }
+});
