52a82d62b76dad8157839d2f60881f43dd734fcb
Catalin Gavrila <cata_bb92@yahoo.com>
2024-12-15 15:18:34 +0200

    big update, added product page template and some other changes

--- script.js
+++ script.js
@@ -140,3 +140,16 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 //Banner images rotation end
+
+// Image switching for product page
+
+const thumbnails = document.querySelectorAll('.thumbnail');
+const mainImage = document.getElementById('main-image');
+
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', () => {
+        mainImage.src = thumbnail.src;
+    });
+});
+
+// Image switching for product page end
